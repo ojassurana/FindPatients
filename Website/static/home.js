7e899340a5cdbdde45bf70361162a18038cc92d3
InFInItyKiLL33 <2poh.junkang@gmail.com>
2021-06-05 18:31:15 +0800

    fixed mobile results

--- Website/static/home.js
+++ Website/static/home.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         if (creditState == 1) {
             creditState = 2;
             $(".text-6").html("Contact Us");
-            $(".text-5").html("Made by Ojas Surana, <a href='mailto:contact@sreeramvasanth.com' >Sreeram Vasanth<a>, Nihaal Manaf and Poh Jun Kang");
+            $(".text-5").html("Made by Ojas Surana, <a href='mailto:contact@sreeramvasanth.com'>Sreeram Vasanth</a>, Nihaal Manaf and Poh Jun Kang");
         } else {
             creditState = 1;
             $(".text-6").html("Credits");
